ef83da998e0cae9b3d3f6a8d0573e2740bcaeeea
Arjay Bracero <ardhenbracero@gmail.com>
2022-09-14 21:38:57 +0800

    testimonials

---
+++ resources/views/components/testimonials.blade.php
@@ -0,0 +1,109 @@
+
+
+<section class="section testimonial">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="heading">
+					<h2 class="mb-4">What they say about us</h2>
+					<p>They are truely amazing in the industry, Satisfied with their work. Surely recommended. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12 testimonial-wrap">
+				<div class="testimonial-block">
+					<p>
+						I would like to thank you Mr. Henry and the rest of your team for taking care of my
+						debut party. Your teams
+						did an excellent job in filming the event. May God bless all of you always and more power to
+						your company.
+					</p>
+
+					<div class="client-info d-flex align-items-center">
+						<div class="client-img">
+							<img src="{{asset('images/team/testimonial-one.webp')}}" alt="" class="img-fluid">
+						</div>
+						<div class="info">
+							<h6>Kate Garcia</h6>
+							<span>Solena Tuguegarao</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="testimonial-block">
+					<p>
+						Thank you so much Photocity for making our wedding a magical! Two thumbs up for all the staff
+						and crew, you made us feel celebrity on
+						our wedding day, Thanks for you full support guys you grant all of our request and demands.
+					</p>
+
+					<div class="client-info d-flex align-items-center">
+						<div class="client-img">
+							<img src="{{asset('images/team/testimonial-two.webp')}}" alt="" class="img-fluid">
+						</div>
+						<div class="info">
+							<h6>Andrew Reyes</h6>
+							<span>Isabela</span>
+						</div>
+					</div>
+				</div>
+				<div class="testimonial-block">
+					<p>
+						We want to start by thanking God for what we have now and of course special thanks to Photocity
+						for making our dream wedding come true.
+						We recommend our awesome coordinator kahit nasa malayo kami naging perfect ang wedding namin.
+					</p>
+
+					<div class="client-info d-flex align-items-center">
+						<div class="client-img">
+							<img src="{{asset('images/team/testimonial-five.webp')}}" alt="" class="img-fluid">
+						</div>
+						<div class="info">
+							<h6>Liebel Delacruz</h6>
+							<span>Isabela City</span>
+						</div>
+					</div>
+				</div>
+				<div class="testimonial-block">
+					<p>
+						First time renting the Photocity and I must say the experience was terrific. The team was great
+						to work with and could not be any nicer.
+						They are fully equipped with proffesional lights. Looks like I found long term studio to work
+						with.
+					</p>
+
+					<div class="client-info d-flex align-items-center">
+						<div class="client-img">
+							<img src="{{asset('images/team/testimonial-six.webp')}} "alt="" class="img-fluid">
+						</div>
+						<div class="info">
+							<h6>Catherine Delosantos</h6>
+							<span>Nueva Viscaya</span>
+						</div>
+					</div>
+				</div>
+				<div class="testimonial-block">
+					<p>
+						Very accomodating team, gets the look and feel of what you want, easy to coordinate with! (They
+						give you no hassle whatsoever) Rest
+						assured, your moments are captured a hight qaulity and good memories in the future, 5/5.
+					</p>
+
+					<div class="client-info d-flex align-items-center">
+						<div class="client-img">
+							<img src="{{asset('images/team/testimonial-four.webp')}}" alt="" class="img-fluid">
+						</div>
+						<div class="info">
+							<h6>Erna Joyce</h6>
+							<span>Ilagan City</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
